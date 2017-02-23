694c01d80a5e71fd10d385b0aec2efec4c2d9b0f
kimbud4net <kim@bud4.net>
2017-02-23 17:55:47 +0800

    Add project files.

---
+++ AppConfigTransformLab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConfigTransformLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start:" + DateTime.Now);
+            Console.WriteLine(ConfigurationManager.AppSettings["Stage"]);
+            Console.WriteLine(ConfigurationManager.ConnectionStrings["Name"].ConnectionString);
+            Console.WriteLine("End:" + DateTime.Now);
+
+            Console.Read();
+        }
+    }
+}
